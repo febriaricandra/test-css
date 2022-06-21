3551af3796ca86ad2c175bae5f5aa18ee900279e
febriaricandra <febriaricandraproduction@gmail.com>
2022-06-21 12:58:50 +0700

    test css

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { CityNews } from './CityNews';
+
+function App() {
+  return (
+    <div className="App">
+      <CityNews />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/CityNews.jsx
@@ -0,0 +1,61 @@
+import "./css/citynews.css";
+
+export const CityNews = () => {
+  return (
+    <div className="container">
+      <div className="city-news-div">City News</div>
+      <div className="flex-container">
+      <div className="component-1-div">
+        <div className="rectangle-div" />
+        <div className="rectangle-div1" />
+        <img className="ellipse-icon" alt="" src="./ellipse-1.svg" />
+        <img className="ellipse-icon1" alt="" src="./ellipse-2@2x.png" />
+        <b className="andrew-b">Andrew</b>
+        <div className="nov-div">12 nov</div>
+        <div className="want-to-level-up-your-exploita">
+          Want to level up your exploitation, detection, networking, and
+          mitigation skills to keep pace with today's most critical security
+          threats?
+        </div>
+      </div>
+      <div className="component-2-div">
+        <div className="rectangle-div2" />
+        <div className="rectangle-div3" />
+        <img className="ellipse-icon2" alt="" src="ellipse-2@2x.png" />
+        <b className="andrew-b1">Andrew</b>
+        <div className="nov-div1">12 nov</div>
+        <div className="want-to-level-up-your-exploita1">
+          Want to level up your exploitation, detection, networking, and
+          mitigation skills.
+        </div>
+      </div>
+      <div className="component-3-div">
+        <div className="rectangle-div2" />
+        <div className="rectangle-div3" />
+        <img className="ellipse-icon2" alt="" src="ellipse-2@2x.png" />
+        <b className="andrew-b1">Andrew</b>
+        <div className="nov-div1">12 nov</div>
+        <div className="want-to-level-up-your-exploita1">
+          Want to level up your exploitation, detection, networking, and
+          mitigation skills.
+        </div>
+      </div>
+      <div className="component-5-div">
+        <div className="component-4-div">
+          <div className="rectangle-div6" />
+          <div className="rectangle-div7" />
+          <div className="rectangle-div8" />
+          <div className="post-news-div">Post news</div>
+        </div>
+        <img className="ellipse-icon4" alt="" src="ellipse-2@2x.png" />
+        <b className="andrew-b3">Andrew</b>
+        <div className="nov-div3">12 nov</div>
+        <div className="want-to-level-up-your-exploita3">
+          Want to level up your exploitation, detection, networking, and
+          mitigation skills.
+        </div>
+      </div>
+      </div>
+    </div>
+  );
+};
